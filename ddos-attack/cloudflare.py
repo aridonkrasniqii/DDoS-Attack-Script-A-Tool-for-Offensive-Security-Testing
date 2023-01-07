e271db5b26d4f111bbb8684ad2342d4bb115e3d7
BlerandaAzemi <95053606+Blesaza@users.noreply.github.com>
2023-01-07 11:20:27 +0100

    Create cloudflare.py

---
+++ ddos-attack/cloudflare.py
@@ -0,0 +1,12 @@
+import urllib.request
+
+# Function that tells if website is protected by cloudflare
+
+def cloudflare(url):
+    cf_message = False
+    req = urllib.request.Request(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'})
+    response = urllib.request.urlopen(req)
+    if "CF-Cache-Status: HIT" in str(response.info()):
+        cf_message = True
+    return cf_message
