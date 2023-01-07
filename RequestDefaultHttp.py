3cfa214bc790631da7d9eede498650e561877b3d
BlerandaAzemi <95053606+Blesaza@users.noreply.github.com>
2023-01-07 01:26:25 +0100

    Create RequestDefaultHttp.py

---
+++ RequestDefaultHttp.py
@@ -0,0 +1,25 @@
+
+import threading
+
+class RequestDefaultHTTP(threading.Thread):
+    go = threading.Event
+    def __init__(self, counter):
+
+        threading.Thread.__init__(self)
+        self.counter = counter
+
+    def run(self, go=None):
+        go.wait()
+        while True:
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.socket(str(args.host), int(args.port))
+                s.send(str.encode(request))
+                print("Request is sent: ", self.counter)
+                try:
+                    for y in range(150):
+                        s.send(str.encode(request))
+                except:
+                    s.close()
+            except:
+                s.close()
